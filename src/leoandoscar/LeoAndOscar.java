fdd8567bd57eeb8358c5fa86f6ccc02113fba0ae
PUTRA <msabilshahputra@gmail.com>
2024-05-14 10:09:09 +0700

    CODEWARS

---
+++ src/leoandoscar/LeoAndOscar.java
@@ -0,0 +1,13 @@
+
+package leoandoscar;
+
+public class LeoAndOscar {
+public class Kata{
+  public static String leo(final int oscar) {
+    if (oscar == 88) return "Leo finally won the oscar! Leo is happy";
+    if (oscar == 86) return "Not even for Wolf of wallstreet?!";
+    if (oscar < 88) return "When will you give Leo an Oscar?";
+    else return "Leo got one already!";
+  }
+    }
+}
